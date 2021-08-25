d881e778c1efe8f8ba24dfc9d453745fba1deb85
Baharudin78 <bencoxgame78@gmail.com>
2021-08-25 23:04:53 +0700

    implement UI

--- app/src/main/java/com/baharudin/coronainfo/MainActivity.kt
+++ app/src/main/java/com/baharudin/coronainfo/MainActivity.kt
@@ -2,10 +2,21 @@ package com.baharudin.coronainfo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.baharudin.coronainfo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navigation = supportFragmentManager.findFragmentById(R.id.fragmentContainer) as NavHostFragment
+        navController = navigation.navController
     }
 }
